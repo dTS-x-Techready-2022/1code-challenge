f4a84a4db323ac84d0549f6fd11c0ddd5c945377
vickyvaf <vickyadi243@gmail.com>
2022-09-27 21:33:26 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/Header/Header";
+import Banner from "./components/content/Banner";
+
+import Container from "@mui/material/Container";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <Banner />
+      </Container>
+    </>
+  );
+}
+
+export default App;
