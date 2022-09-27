6dc055df0efd85527365d62ba45b6c742ab2207d
trianggautama <trianggategarutama@gmail.com>
2022-09-27 22:32:10 +0800

    react js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Banner from './components/Banner';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
